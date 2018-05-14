ef09274ccd776a2d5d2d1e9b7dc3bfef63676f4e
Javier Olaechea <pirata@gmail.com>
2018-05-14 08:38:42 -0500

    Stub In-memory table with fixed schema

--- bsql.h
+++ bsql.h
@@ -19,3 +19,20 @@ typedef enum statement_t statement_t;
 typedef struct {
   statement_t type;
 } Statement;
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct {
+  size_t id;
+  char username[COLUMN_USERNAME_SIZE];
+  char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+typedef struct {
+  void *pages[TABLE_MAX_PAGES];
+    size_t num_rows;
+} Table;
+
+void serialize_row(Row*, void*);
+void deserialize_row(void*, Row*);
