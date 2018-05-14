db9685bb658d410dc5fde09d211e157c34bdfe9d
Javier Olaechea <pirata@gmail.com>
2018-05-13 19:09:24 -0500

    Add meta_commands

--- bsql.c
+++ bsql.c
@@ -3,20 +3,40 @@
 #include <string.h>
 #include <readline/readline.h>
 
+#include "bsql.h"
+
 char *input;
 char *prompt = "db> ";
+
+
+meta_command_t
+parse_command(char *cmd)
+{
+  if ((strncmp(".exit", cmd, 5) == 0)) {
+      return META_COMMAND_EXIT;
+    }
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+}
 
 int
 main(int argc, char *argv[])
 {
-  input = readline(prompt);
-  if (!input) perror("Couldn't read prompt.");
-  
-  while(!(strncmp(".exit", input, 5) == 0)) {
-    printf("Unrecognized command: %s\n", input);
-
+  for(;;){
     input = readline(prompt);
-    if (!input) perror("Couldn't read prompt.");
+    if (!input) perror("Couldn't read from the REPL.");
+
+    if(input[0] == '.') {
+      meta_command_t command = parse_command(input);
+        switch(command) {
+        case META_COMMAND_EXIT:
+          return EXIT_SUCCESS;
+          break;
+        case META_COMMAND_UNRECOGNIZED_COMMAND:
+          break;
+        }
+    }
+
+    printf("Unrecognized command: %s\n", input);
   }
 
   return EXIT_SUCCESS;
---
+++ bsql.h
@@ -0,0 +1,6 @@
+/* MetaCommands start with . */
+enum meta_command_t {
+  META_COMMAND_EXIT,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+};
+typedef enum meta_command_t meta_command_t;
